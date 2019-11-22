cbfd90f9662430ab4e0852c3bffde3c785187262
jjarl12 <jonathan-jarl@bethel.edu>
2019-11-21 19:22:15 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is the test site
+          This is the test site Demo 2
         </p>
         <a
           className="App-link"
