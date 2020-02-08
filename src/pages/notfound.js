994f4d68b2daa9420c2e7cef16d324203e87ec38
serhiy1017 <s.bezpalyi@yandex.com>
2020-02-08 19:35:01 +0200

    modified the 404 page. it wil be redirected to the home page.

--- src/pages/notfound.js
+++ src/pages/notfound.js
@@ -1,17 +1,34 @@
 import React, {Component} from "react";
 import "../css/404.css";
+import {Link, Redirect} from "react-router-dom";
 
 class Notfound extends Component{
+	constructor(){
+		super();
+
+		this.state = {
+			goingHome: false,
+		}
+	}
+
 	render(){
+		setTimeout(() => {
+			this.setState({goingHome: true})
+		}, 5000);
 		return (
-			<main>
-				<div className="notfound-body">
-					<div className="w3-display-middle w3-text-gray">
-						<h1 className={"jumbo-title"}>404</h1>
-						<p>Oops, :(<br/> the page was not found.</p>
+			this.state.goingHome ?
+				<Redirect to={"/"}/>
+				:
+				<main>
+					<div className="notfound-body">
+						<div className="w3-display-middle w3-text-gray">
+							<h1 className={"jumbo-title"}>404</h1>
+							<p className={"w3-small"}>Oops, the page was not found.</p>
+							<p>This page will be <strong className={"w3-xlarge w3-text-purple"}>redirected</strong> to the HOME page <strong className={"w3-large w3-text-purple"}>automatically</strong>.</p>
+							<p>Or you can click <Link to={"/"}><span className={"w3-text-purple w3-xxlarge"}>here</span></Link> directly.</p>
+						</div>
 					</div>
-				</div>
-			</main>
+				</main>
 		);
 	}
 }
