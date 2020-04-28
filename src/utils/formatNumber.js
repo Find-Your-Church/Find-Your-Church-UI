18d56cc8d6616ac01f738787a51a51982dafc05e
serhiy1017 <s.bezpalyi@yandex.com>
2020-04-28 14:42:13 +0300

    create/update subscription.

--- src/utils/formatNumber.js
+++ src/utils/formatNumber.js
@@ -1,6 +1,6 @@
 
-const formatNumner = (num) => {
+const formatNumber = (num) => {
 	return num.toString().length < 2 ? "0" + num : num;
 };
 
-export default formatNumner;
+export default formatNumber;
