7e42024c3545da6d145ee30f18bd54b06f48f78f
serhiy1017 <s.bezpalyi@yandex.com>
2020-07-19 13:46:47 +0300

    search pagination bug fixed

--- src/utils/addCounts.js
+++ src/utils/addCounts.js
@@ -1,25 +1,23 @@
 const addCounts = (counts1, counts2) => {
-	if(Object.keys(counts2).length === 0)
-		return counts1;
 	let ret_obj = {};
 
 	for(const cnt1 of Object.keys(counts1)){
-		if(ret_obj[cnt1]){
-			ret_obj[cnt1] += counts1[cnt1];
-		}
-		else{
-			ret_obj = {...ret_obj, [cnt1]: counts1[cnt1]}
-		}
+		ret_obj[cnt1] = [...counts1[cnt1]];
 	}
+
+	console.log(ret_obj);
+	console.log(counts2);
 
 	for(const cnt2 of Object.keys(counts2)){
 		if(ret_obj[cnt2]){
-			ret_obj[cnt2] += counts1[cnt2];
+			for(let i = 0; i < counts2[cnt2].length; i++)
+				ret_obj[cnt2][i] += counts2[cnt2][i];
 		}
 		else{
-			ret_obj = {...ret_obj, [cnt2]: counts1[cnt2]}
+			ret_obj[cnt2] = [...counts1[cnt2]];
 		}
 	}
+	console.log(ret_obj);
 
 	return ret_obj;
 };
